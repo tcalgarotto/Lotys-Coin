008cb5a4f650dd005a281ebb968c321eabd3545c
tcalgarotto <128938248+tcalgarotto@users.noreply.github.com>
2023-09-26 12:08:22 -0300

    LOTS.sol

--- Lotys Coin $LOTS/LOTS.sol
+++ Lotys Coin $LOTS/LOTS.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "Lotys Coin $LOTS/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {address public myWallet = 0x5F83d6859eFA85Eec810A1f842ACE909659f029f;
     constructor() ERC20("Lotys Coin", "LOTS") {
